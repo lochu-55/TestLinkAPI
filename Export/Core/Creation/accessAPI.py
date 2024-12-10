ab70ec0c9897e3c9c7bcdb63ed6d1f20f5a0d6bb
varalaxmi ganta <varalaxmiganta066@gmail.com>
2024-12-10 13:21:50 +0530

    added logging and exceptions

--- Export/Core/Creation/accessAPI.py
+++ Export/Core/Creation/accessAPI.py
@@ -1,50 +1,132 @@
 from testlink import TestlinkAPIClient
-from bs4 import BeautifulSoup
+import logging
 
+open('Utils/testlink_export.log', 'w').close()
+# Set up logging
+logging.basicConfig(filename='testlink_export.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(console_format)
+
+logger.addHandler(console_handler)
 
 class TestLinkManager:
     def __init__(self, url, api_key):
-        self.testlink = TestlinkAPIClient(url, api_key)
+        try:
+            self.testlink = TestlinkAPIClient(url, api_key)
+            logger.info("TestLink API Client initialized successfully.")
+        except Exception as e:
+            logger.error(f"Failed to initialize TestLink API Client: {e}")
+            raise
 
     # API Interaction Methods
     def get_projects(self):
-        return self.testlink.getProjects()
+        try:
+            projects = self.testlink.getProjects()
+            logger.info(f"Fetched {len(projects)} projects.")
+            return projects
+        except Exception as e:
+            logger.error(f"Error fetching projects: {e}")
+            raise
 
-    def get_project_test_plans(self, project_id):
-        return self.testlink.getProjectTestPlans(project_id)
+    def get_project_test_plans(self, project_id,project_name):
+        try:
+            test_plans = self.testlink.getProjectTestPlans(project_id)
+            logger.info(f"      Fetched {len(test_plans)} test plans for project {project_name}.")
+            return test_plans
+        except Exception as e:
+            logger.error(f"     Error fetching test plans for project {project_name}: {e}")
+            raise
 
     def get_test_suites_for_test_plan(self, plan_id):
-        return self.testlink.getTestSuitesForTestPlan(plan_id)
+        try:
+            test_suites = self.testlink.getTestSuitesForTestPlan(plan_id)
+            logger.info(f"      Fetched {len(test_suites)} test suites for plan {plan_id}.")
+            return test_suites
+        except Exception as e:
+            logger.error(f"     Error fetching test suites for test plan {plan_id}: {e}")
+            raise
 
     def get_test_cases_for_test_plan(self, plan_id):
-        return self.testlink.getTestCasesForTestPlan(plan_id)
+        try:
+            test_cases = self.testlink.getTestCasesForTestPlan(plan_id)
+            logger.info(f"      Fetched {len(test_cases)} test cases for plan {plan_id}.")
+            return test_cases
+        except Exception as e:
+            logger.error(f"     Error fetching test cases for test plan {plan_id}: {e}")
+            raise
 
     def get_test_case_details(self, case_id):
-        return self.testlink.getTestCase(case_id)
+        try:
+            case_details = self.testlink.getTestCase(case_id)
+            logger.info(f"      Fetched details for test case {case_id}.")
+            return case_details
+        except Exception as e:
+            logger.error(f"     Error fetching details for test case {case_id}: {e}")
+            raise
 
-    def fetch_requirements(self,project_id):
-        requirements = self.testlink.getRequirements(project_id)
-        if requirements:
-            req_doc_ids = []
-            for req in requirements:
-                req_doc_ids.append(req['req_doc_id'])
-            return req_doc_ids
 
-        else:
-            return []
+    def fetch_requirements(self, project_id,project_name):
+        try:
+            requirements = self.testlink.getRequirements(project_id)
+
+            if requirements:
+                req_doc_ids = [req['req_doc_id'] for req in requirements]
+                logger.info(f"      Fetched {len(req_doc_ids)} requirements for project {project_name}.")
+                return req_doc_ids
+            else:
+                logger.info(f"      No requirements found for project {project_name}.")
+                return []
+        except Exception as e:
+            logger.error(f"     Error fetching requirements for project {project_name}: {e}")
+            raise
 
     def fetch_testcase_keywords(self, testcaseid, externalid):
-        keywords = self.testlink.getTestCaseKeywords(testcaseid=testcaseid, testcaseexternalid=externalid)
-        keywords = keywords[testcaseid]
-        if isinstance(keywords,dict):
-            val = keywords.values()
-            val = ', '.join(val)
-            return val
-        else:
-            return " "
+        try:
+            keywords = self.testlink.getTestCaseKeywords(testcaseid=testcaseid, testcaseexternalid=externalid)
+            keywords = keywords.get(testcaseid, {})
+            if isinstance(keywords, dict):
+                val = ', '.join(keywords.values())
+                logger.info(f"      Fetched keywords for test case {testcaseid}: {val}")
+                return val
+            else:
+                logger.info(f"      No keywords found for test case {testcaseid}.")
+                return " "
+        except Exception as e:
+            logger.error(f"     Error fetching keywords for test case {testcaseid}: {e}")
+            raise
 
+    def get_status(self, testcasedetails):
+        try:
+            status_dict = {'1': 'Draft', '2': 'Ready for review', '3': 'Review in progress', '4': 'Rework', '5': 'Obsolete', '6': 'Future', '7': 'Final'}
+            status = status_dict.get(testcasedetails, "Unknown Status")
+            logger.info(f"      Test case status: {status}")
+            return status
+        except Exception as e:
+            logger.error(f"     Error getting status for test case {testcasedetails}: {e}")
+            raise
 
+    def get_importance(self, testcasedetails):
+        try:
+            imp_dict = {'1': 'High', '2': 'Medium', '3': 'Low'}
+            importance = imp_dict.get(testcasedetails, "Unknown Importance")
+            logger.info(f"      Test case importance: {importance}")
+            return importance
+        except Exception as e:
+            logger.error(f"     Error getting importance for test case {testcasedetails}: {e}")
+            raise
 
+    def get_execution_type(self, testcasedetails):
+        try:
+            imp_dict = {'1': 'Manual', '2': 'Automated'}
+            execution_type = imp_dict.get(testcasedetails, "Unknown Execution Type")
+            logger.info(f"      Test case execution type: {execution_type}")
+            return execution_type
+        except Exception as e:
+            logger.error(f"     Error getting execution type for test case {testcasedetails}: {e}")
+            raise
 
 
 
--- Export/Core/Creation/export_excel.py
+++ Export/Core/Creation/export_excel.py
@@ -1,33 +1,53 @@
-from accessAPI import TestLinkManager
+from accessAPI import TestLinkManager,logger
 import pandas as pd
-import re
+from re import sub
+
 
 class ExportToExcel:
     def __init__(self, testlink_manager):
         self.testlink_manager = testlink_manager
 
+
     def collect_projects_and_test_cases(self):
         data = []
-        projects = self.testlink_manager.get_projects()
+        try:
+            projects = self.testlink_manager.get_projects()
+            logger.info("Successfully fetched projects from TestLink.")
+        except Exception as e:
+            logger.error(f"Failed to fetch projects: {e}")
+            return []
 
         for project in projects:
-            project_name = project['name']
+            project_name = project.get('name')
+            logger.info(f"   Project  : {project_name}")
             p1 = True  # Flag to ensure project name appears only once
 
-            requirements = self.testlink_manager.fetch_requirements(project['id'])
-            test_plans = self.testlink_manager.get_project_test_plans(project['id'])
+            try:
+                requirements = self.testlink_manager.fetch_requirements(project['id'],project_name)
+                test_plans = self.testlink_manager.get_project_test_plans(project['id'],project_name)
+            except Exception as e:
+                logger.error(f"     Failed to fetch requirements or test plans for project {project_name}: {e}")
+                continue
 
             for plan in test_plans:
-                plan_name = plan['name']
+                plan_name = plan.get('name')
                 plan_flag = True  # Flag to ensure test plan name appears only once
 
-                test_suites = self.testlink_manager.get_test_suites_for_test_plan(plan['id'])
+                try:
+                    test_suites = self.testlink_manager.get_test_suites_for_test_plan(plan['id'])
+                except Exception as e:
+                    logger.error(f"     Failed to fetch test suites for plan {plan_name}: {e}")
+                    continue
 
                 for suite in test_suites:
-                    suite_name = suite['name']
+                    suite_name = suite.get('name', 'Unknown Suite')
                     suite_flag = True  # Flag to ensure test suite name appears only once
 
-                    test_cases = self.testlink_manager.get_test_cases_for_test_plan(plan['id'])
+                    try:
+                        test_cases = self.testlink_manager.get_test_cases_for_test_plan(plan['id'])
+                    except Exception as e:
+                        logger.error(f"     Failed to fetch test cases for suite {suite_name}: {e}")
+                        continue
 
                     for case_id, case_data in test_cases.items():
                         if isinstance(case_data, dict):
@@ -54,13 +74,15 @@ class ExportToExcel:
 
     def _process_test_case(self, data, project_name, plan_name, suite_name,
                            p1, plan_flag, suite_flag, case_data, requirements):
-
-        test_case_details = self.testlink_manager.get_test_case_details(case_data['tcase_id'])
-        tc_keywords = self.testlink_manager.fetch_testcase_keywords(case_data['tcase_id'], case_data['external_id'])
+        try:
+            test_case_details = self.testlink_manager.get_test_case_details(case_data['tcase_id'])
+            tc_keywords = self.testlink_manager.fetch_testcase_keywords(case_data['tcase_id'], case_data['external_id'])
+        except Exception as e:
+            logger.error(f"     Failed to fetch details or keywords for test case {case_data['tcase_name']}: {e}")
+            return
 
         if isinstance(test_case_details, list) and len(test_case_details) > 0:
             test_case = test_case_details[0]
-
             preconditions = test_case.get('preconditions', '')
             summary = test_case.get('summary', '')
 
@@ -68,22 +90,21 @@ class ExportToExcel:
             if isinstance(steps, str):
                 # Steps as a single string
                 data.append({
-                    'Project': project_name if p1 else "",
+                    'Test Project Name': project_name if p1 else "",
                     'Test Plan': plan_name if plan_flag else "",
                     'Test Suite': suite_name if suite_flag else "",
-                    'Test Case ID': case_data['tcase_id'],
-                    'Test Case Name': case_data['tcase_name'],
+                    'Test Case Title': case_data['tcase_name'],
                     'Summary': summary,
                     'Preconditions': preconditions,
                     'Step Actions': steps,
                     'Step Expected Results': steps,
                     'Step Execution Type': steps,  # Assuming execution type is the same as steps
-                    'Step Execution Status': case_data['exec_status'],
-                    'Test Execution Status': test_case.get('status', " "),
-                    'TC_Keywords': tc_keywords,
+                    'Test Execution Status': self.testlink_manager.get_status(test_case.get('status', " ")),
+                    'Test Execution Type': self.testlink_manager.get_execution_type(test_case.get('execution_type')),
+                    'Test Case Keywords': tc_keywords,
                     'Execution Order': case_data['execution_order'],
                     'Requirements': ", ".join(requirements),
-                    'Importance': test_case.get('importance', " "),
+                    'Importance': self.testlink_manager.get_importance(test_case.get('importance', " ")),
                     'Estimated Duration': test_case.get('estimated_exec_duration', " "),
                     'Platform': case_data.get('platform_name', " ")
                 })
@@ -97,7 +118,6 @@ class ExportToExcel:
     def _process_steps(self, data, project_name, plan_name, suite_name,
                        p1, plan_flag, suite_flag, case_data, test_case, steps,
                        summary, preconditions, requirements, tc_keywords):
-
         steps_data = ""
         expected_result = ""
         exec_type = ""
@@ -114,41 +134,46 @@ class ExportToExcel:
         for index, step_text in enumerate(steps_lines):
             if index < len(expected_result_lines) and step_text:
                 data.append({
-                    'Project': project_name if (index == 0 and p1) else "",
+                    'Test Project Name': project_name if (index == 0 and p1) else "",
                     'Test Plan': plan_name if (index == 0 and plan_flag) else "",
                     'Test Suite': suite_name if (index == 0 and suite_flag) else "",
-                    'Test Case ID': case_data['tcase_id'] if index == 0 else "",
-                    'Test Case Name': case_data['tcase_name'] if index == 0 else "",
+                    'Test Case Title': case_data['tcase_name'] if index == 0 else "",
                     'Summary': self.remove_html_tags(summary) if index == 0 else "",
                     'Preconditions': self.remove_html_tags(preconditions) if index == 0 else "",
                     'Step Actions': self.remove_html_tags(step_text),
                     'Step Expected Results': self.remove_html_tags(expected_result_lines[index]) if index < len(expected_result_lines) else "",
-                    'Step Execution Type': exec_type_lines[index] if index < len(exec_type_lines) else "",
-                    'Step Execution Status': case_data['exec_status'] if index == 0 else "",
-                    'Test Execution Status': test_case.get('status', " ") if index == 0 else "",
-                    'TC_Keywords': tc_keywords if index == 0 else "",
+                    'Step Execution Type': self.testlink_manager.get_execution_type(exec_type_lines[index]) if index < len(exec_type_lines) else "",
+                    'Test Execution Status': self.testlink_manager.get_status(test_case.get('status', " ")) if index == 0 else "",
+                    'Test Execution Type': self.testlink_manager.get_execution_type(test_case.get('execution_type')) if index == 0 else " ",                    'Test Case Keywords': tc_keywords if index == 0 else "",
                     'Execution Order': case_data['execution_order'] if index == 0 else "",
                     'Requirements': ", ".join(requirements) if index == 0 else "",
-                    'Importance': test_case.get('importance', " ") if index == 0 else "",
+                    'Importance': self.testlink_manager.get_importance(test_case.get('importance', " ")) if index == 0 else "",
                     'Estimated Duration': test_case.get('estimated_exec_duration', " ") if index == 0 else "",
                     'Platform': case_data.get('platform_name', " ") if index == 0 else ""
                 })
 
     def remove_html_tags(self, text):
-
-        clean_text = re.sub(r'<.*?>', '', text)  # Regex to remove HTML tags
-        return clean_text
+        try:
+            clean_text = sub(r'<.*?>', '', text)  # Regex to remove HTML tags
+            return clean_text
+        except Exception as e:
+            logger.error(f"     Failed to remove HTML tags from text: {e}")
+            return text
 
     def export_to_excel(self, data, filename="Excelsheet/test_cases.xlsx"):
-        df = pd.DataFrame(data)
-        df.to_excel(filename, index=False)
+        try:
+            df = pd.DataFrame(data)
+            df.to_excel(filename, index=False)
+            logger.info(f"Data successfully exported to {filename}")
+        except Exception as e:
+            logger.error(f"Failed to export data to Excel: {e}")
 
 
 if __name__ == "__main__":
     # URL and API key for TestLink
-    url = "http://172.17.17.8:8085/lib/api/xmlrpc/v1/xmlrpc.php"
-    api_key = "10b2132073a17c9d4a0bc700dd778f83"
-
+    url = "http://172.17.17.93:8085/lib/api/xmlrpc/v1/xmlrpc.php"
+    api_key = "6e6ac11d3cd05d79f5d2d07e7338e0c0"
+    #api_key = "10b2132073a17c9d4a0bc700dd778f83"
     # Create an instance of TestLinkManager
     testlink_manager = TestLinkManager(url, api_key)
 
@@ -160,3 +185,4 @@ if __name__ == "__main__":
 
     # Export data to Excel
     exporter.export_to_excel(data)
+
